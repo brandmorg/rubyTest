e120cc260c549071c1838d234d6a768c22026efa
Branden Morgan <morgan.fnf@gmail.com>
2023-05-27 18:23:28 -0400

    creating classes.

--- models.rb
+++ models.rb
@@ -3,3 +3,14 @@
 # Menu and MenuItem have typical data associated with restaurants. 
 # Illustrate behavior via unit tests.
 
+Menu has_many MenuItems 
+
+MenuItems belongs_to Menu
+
+class Menu < Application Record
+    has_many :menu_items
+end
+
+class MenuItem < ApplicationRecord
+    belongs_to :Menu
+end
