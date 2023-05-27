79904ecf102b8c46042a24fdd716f782227ee42a
Branden Morgan <morgan.fnf@gmail.com>
2023-05-27 18:25:27 -0400

    adding structure

---
+++ App.rb
@@ -0,0 +1,5 @@
+# Create an object model for Menu and MenuItem(s) classes.
+# Menu has many MenuItems(s)
+# Menu and MenuItem have typical data associated with restaurants. 
+# Illustrate behavior via unit tests.
+
---
+++ Models/Menu.rb
@@ -0,0 +1,3 @@
+class Menu < Application Record
+    has_many :menu_items
+end
