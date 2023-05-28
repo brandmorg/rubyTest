41d4f5821c321060a24b1a806ecec67197e4805a
Branden Morgan <morgan.fnf@gmail.com>
2023-05-27 21:54:02 -0400

    Tests are working, data passed in just bad. Need to tweak.

--- devTestApp/spec/models/restaurant_spec.rb
+++ devTestApp/spec/models/restaurant_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type => :model do
+RSpec.describe Restaurant, :type => :model do
     it { should have_many(:Menu)} # Confirm has many Menu(s)
     it { should it { should have_many(:MenuItem).through(:Menu)}}  # Confirm has many MenuItem(s) through Menu(s)
     it { should it { should validate_presence_of(:name)}} # Confirm name not null. 
