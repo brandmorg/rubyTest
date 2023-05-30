3abe8484557d098e0795818ef38f32f57829fcec
Branden Morgan <morgan.fnf@gmail.com>
2023-05-30 19:53:48 -0400

    Maybe if I didn't bork RSpec on the test class

--- devTestApp/spec/models/menu_spec.rb
+++ devTestApp/spec/models/menu_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe Menu, :type => :model do
+RSpec.describe Menu, :type => :model do
     it { should have_many(:MenuItem) } # Confirm has many MenuItem(s)
     it { should belongs_to(:Restaurant) } # Confirm belongs to Restaurant
     it { should validate_presence_of(:name) } # Confirm name not null
