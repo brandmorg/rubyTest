d7dbedcbdcd8141e56387ad334cb01e1167e93a2
Branden Morgan <morgan.fnf@gmail.com>
2023-05-30 19:03:03 -0400

    Trying to get tests working. No DB might mean table search isn't going to work? Need to research ruby more.

--- devTestApp/spec/models/menu_spec.rb
+++ devTestApp/spec/models/menu_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Menu, :type => :model do
-    it { should have_many(:MenuItem)} # Confirm has many MenuItem(s)
-    it { should belongs_to(:Restaurant)} # Confirm belongs to Restaurant
-    it { should it { should validate_presence_of(:name)}} # Confirm name not null
+ describe Menu do
+    it { should have_many(:MenuItem) } # Confirm has many MenuItem(s)
+    it { should belongs_to(:Restaurant) } # Confirm belongs to Restaurant
+    it { should validate_presence_of(:name) } # Confirm name not null
+
 end
