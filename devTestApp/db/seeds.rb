7159f8d21a6d5969fb66e3efee7427e003979cb3
Branden Morgan <morgan.fnf@gmail.com>
2023-05-30 19:16:35 -0400

    This might be the path to getting the tables? More learning

--- devTestApp/db/seeds.rb
+++ devTestApp/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+menus = Menu.create(name: "TestMenu")
+menuitems = MenuItem.create(name: "Tacos", price: "$0.99")
+restaurants = Restaurant.create(name: "Test Place")
+
